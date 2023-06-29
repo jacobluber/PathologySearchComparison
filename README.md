03d6e685de4fcb7e4efd61bb4764f5764597eb16
Mohammad Sadegh Nasr <38806748+SadeghNasr@users.noreply.github.com>
2023-06-29 15:51:10 -0400

    yottixel updated

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Pathology Search Comparison
+In order to run each of the models, you should change the working directory to the corresponding directory (e.g. `cd yottixel`).
 
 ## database
 Here, you can find two notebooks: "gdc_search.ipynb" and "sample.ipynb". "gdc_search.ipynb" uses NCI's GDC API to retrieve all the .SVS slides in TCGA project that have a `primary_site` of rither "Breast", "Brain", "Bronchus and lung", "Colon", or "Liver and intrahepatic bile ducts". "sample.ipynb" would randomly sample 50 to 75 slides from each category to create the dataset that would be used to create the databases of each method. This sampled dataset is stored as "sampled_metadata.csv".
 
 ## Yottixel
+First you have to generate the mosaics from database slides and test slides. To do so, you can use either "patching.py" or "parallel_patching.py". The only difference is that the latter uses parallel CPUs for a faster runtime.
+
+```python
+python patching.py --data_dir [TCGA_DATA_DIR] --metadata_path ./sampled_metadata.csv --save_dir ./PATCHES
+python parallel_patching.py --data_dir [TCGA_DATA_DIR] --metadata_path ./sampled_metadata.csv --save_dir ./PATCHES --num_processes 16
+```
 
 ```python
 python feature_extraction.py --batch_size 256
 ```
 
-```python
-python parallel_patching.py --metadata_path ./sampled_metadata_okay.csv --num_processes 16
-```
+
 ## RetCCL
 
 ```python
---
+++ yottixel/feature_extraction.py
@@ -0,0 +1,89 @@
+# By Abtin Riasatian, email: abtin.riasatian@uwaterloo.ca
+
+# The extract_features function gets a patch directory and a feature directory.
+# the function will extract the features of the patches inside the folder
+# and saves them in a pickle file of dictionary mapping patch names to features.
+# =============================================================
+
+import os
+from os.path import join
+import glob
+import pickle
+import pathlib
+import argparse
+
+import tensorflow as tf
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.layers import GlobalAveragePooling2D, Lambda
+# from tensorflow.keras.applications.densenet import preprocess_input
+from tensorflow.keras.backend import bias_add, constant    
+from skimage import io
+import numpy as np
+from tqdm import tqdm
+
+
+# feature extractor preprocessing function
+def preprocessing_fn(input_batch, network_input_patch_width):
+    org_input_size = tf.shape(input_batch)[1]
+    # standardization
+    scaled_input_batch = tf.cast(input_batch, 'float') / 255.
+    # resizing the patches if necessary
+    resized_input_batch = tf.cond(tf.equal(org_input_size, network_input_patch_width), 
+                                  lambda: scaled_input_batch, 
+                                  lambda: tf.image.resize(scaled_input_batch, (network_input_patch_width, network_input_patch_width)))
+    # normalization, this is equal to tf.keras.applications.densenet.preprocess_input()---------------
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    data_format = "channels_last"
+    mean_tensor = constant(-np.array(mean))
+    standardized_input_batch = bias_add(resized_input_batch, mean_tensor, data_format)
+    standardized_input_batch /= std
+    return standardized_input_batch
+
+# feature extractor initialization function
+def kimianet_feature_extractor(network_input_patch_width, weights_address):
+    dnx = DenseNet121(include_top=False, weights=weights_address, input_shape=(network_input_patch_width, network_input_patch_width, 3), pooling='avg')
+    kn_feature_extractor = Model(inputs=dnx.input, outputs=GlobalAveragePooling2D()(dnx.layers[-3].output))
+    kn_feature_extractor_seq = Sequential([Lambda(preprocessing_fn, arguments={'network_input_patch_width': network_input_patch_width}, input_shape=(None, None, 3), dtype=tf.uint8)])
+    kn_feature_extractor_seq.add(kn_feature_extractor)
+    return kn_feature_extractor_seq
+
+# feature extraction function
+def extract_features(patch_dir, extracted_features_save_adr, network_weights_address, network_input_patch_width, batch_size, img_format):
+    feature_extractor = kimianet_feature_extractor(network_input_patch_width, network_weights_address)
+    all_patches = join(patch_dir, '*', '*.' + img_format)
+    patch_adr_list = [pathlib.Path(x) for x in glob.glob(all_patches)]
+    feature_dict = {}
+    for batch_st_ind in tqdm(range(0, len(patch_adr_list), batch_size)):
+        batch_end_ind = min(batch_st_ind + batch_size, len(patch_adr_list))
+        batch_patch_adr_list = patch_adr_list[batch_st_ind:batch_end_ind]
+        patch_batch = np.array([io.imread(x) for x in batch_patch_adr_list])
+        batch_features = feature_extractor.predict(patch_batch)
+        feature_dict.update(dict(zip([x.parents[0].name + "_" + x.name for x in batch_patch_adr_list], list(batch_features))))
+        with open(extracted_features_save_adr, 'wb') as output_file:
+            pickle.dump(feature_dict, output_file, pickle.HIGHEST_PROTOCOL)
+
+
+parser = argparse.ArgumentParser(description="KimiaNet feature extraction")
+parser.add_argument('--patch_dir', default='./PATCHES/', help='Directory of patches')
+parser.add_argument('--extracted_features_save_adr', default='./extracted_features.pickle', help='Address to save extracted features')
+parser.add_argument('--network_weights_address', default='./checkpoints/KimiaNetKerasWeights.h5', help='Address of network weights')
+parser.add_argument('--network_input_patch_width', type=int, default=1000, help='Width of network input patch')
+parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
+parser.add_argument('--img_format', default='jpg', help='Image format')
+parser.add_argument('--use_gpu', type=bool, default=True, help='Whether to use GPU')
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if args.use_gpu:
+        os.environ['NVIDIA_VISIBLE_DEVICES'] = '0,1'
+        os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+    else:
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    
+    extract_features(args.patch_dir, args.extracted_features_save_adr, args.network_weights_address, args.network_input_patch_width, args.batch_size, args.img_format)
+    
